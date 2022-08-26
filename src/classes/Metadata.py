1b227675234835dbe234ac6d6e8ee50337bfa21d
Dounia <43225305+0xDounia@users.noreply.github.com>
2022-08-26 06:32:35 -0400

    Update Metadata.py to have animation URL

--- src/classes/Metadata.py
+++ src/classes/Metadata.py
@@ -1,9 +1,10 @@
 class Metadata(object):
-    def __init__(self, name, description, edition, date, thumbnail, attributes):
+    def __init__(self, name, description, edition, date, thumbnail, animation_url, attributes):
         self.name = name
         self.description = description
         self.edition = edition
         self.date = date
         self.thumbnail = thumbnail
+        self.animation_url = animation_url
         self.attributes = attributes
     
